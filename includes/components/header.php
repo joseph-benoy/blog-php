0b4c7a381cbad19b194a27d6d0819479328c8a79
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-18 02:27:00 +0530

    navbar ui finished

--- includes/components/header.php
+++ includes/components/header.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">BLOG</a>
+<nav class="navbar navbar-expand-lg navbar-light" id="navbar">
+  <a class="navbar-brand" href="#" id="navbar-brand-btn">BLOG</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+  <i class="bi bi-list"></i>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
@@ -20,8 +20,7 @@
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" id="navbar-search-input">
     </form>
   </div>
 </nav>
