a2c2865d0337151d74e96d4fffa4bff00200badc
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-18 22:25:31 +0530

    added pagination cards to the index page

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
     define('USER','joseph');
     define('PASSWORD','3057');
     define('DATABASE','BLOG');
-    define('POSTS_PER_PAGE',5);
+    define('POSTS_PER_PAGE',6);
     $db_config_array = array(SERVER,USER,PASSWORD,DATABASE);
     spl_autoload_register(function($class){
         $file_name = "class.".strtolower($class).".php";
--- includes/functions/pagination-handler.php
+++ includes/functions/pagination-handler.php
@@ -7,8 +7,27 @@
             $pagination = $db->get_pagination($page_no,POSTS_PER_PAGE);
             $col_count = 0;
             foreach($pagination->posts as $post){
-                if($col_count<0){
-                    echo '<div class="row">s'
+                if($col_count===0){
+                    echo '<div class="row">';
+                }
+                echo '<div class="col-6">
+                <div class="card" style="width: 32rem;">
+                    <img class="card-img-top" src="admin/uploads/cover_images/'.$post['COVER_IMAGE_LOCATION'].'" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">'.$post['TITLE'].'</h5>
+                        <p class="card-text" style="color:rgba(0,0,0,0.7);">'.$post['DESCRIPTION'].'</p>
+                        <a href="post.php?url='.$post['TITLE_SLAG'].'" class="btn btn-primary read-more-btn">Read more</a>
+                    </div>
+                </div>
+            </div>';
+                if($col_count>0){
+                    echo "</div>";
+                }
+                if($col_count===1){
+                    $col_count=0;
+                }
+                else{
+                    $col_count++;
                 }
             }
         }
