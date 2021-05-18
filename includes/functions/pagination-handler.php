bae359a1ef1057a23ebd01e7133b6b3c855e980d
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-18 23:39:03 +0530

    added page numbers html

--- includes/functions/pagination-handler.php
+++ includes/functions/pagination-handler.php
@@ -30,6 +30,26 @@
                     $col_count++;
                 }
             }
+            echo '            
+                <nav aria-label="Page navigation example">   
+                <ul class="pagination justify-content-center">
+                    <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1">        
+                        <span aria-hidden="true">&laquo;</span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                    <a class="page-link" href="#">
+                        <span aria-hidden="true">&raquo;</span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                    </li>
+                </ul>
+                </nav>';
         }
         catch(Exception $error){
             echo "Pagination error : $error->getMessage()<br>";
