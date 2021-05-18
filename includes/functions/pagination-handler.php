353e0ae5273d8e68f16e9fbcacc81fafc8ceb278
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-18 18:00:57 +0530

    get pagination db class modified

--- includes/functions/pagination-handler.php
+++ includes/functions/pagination-handler.php
@@ -1,6 +1,19 @@
 <?php
+    require_once("includes/config.php");
     function display_pagination($page_no)
     {
-        
+        try{
+            $db = new DB(...$GLOBALS['db_config_array']);
+            $pagination = $db->get_pagination($page_no,POSTS_PER_PAGE);
+            $col_count = 0;
+            foreach($pagination->posts as $post){
+                if($col_count<0){
+                    echo '<div class="row">s'
+                }
+            }
+        }
+        catch(Exception $error){
+            echo "Pagination error : $error->getMessage()<br>";
+        }
     }
 ?>
